71a7e9c47b99d273fd466d9f4f8435d812f202b4
PhiBo <phibo@dinotools.org>
2014-12-19 01:26:14 +0100

    example - Add first example

---
+++ examples/get_ways.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import overpy
+
+api = overpy.Overpass()
+
+# fetch all ways and nodes
+result = api.query("""
+    way(50.746,7.154,50.748,7.157) ["highway"];
+    (._;>;);
+    out body;
+    """)
+
+for way in result.ways:
+    print("Name: %s" % way.tags.get("name", "n/a"))
+    print("  Highway: %s" % way.tags.get("highway", "n/a"))
+    print("  Nodes:")
+    for node in way.nodes:
+        print("    Lat: %f, Lon: %f" % (node.lat, node.lon))
