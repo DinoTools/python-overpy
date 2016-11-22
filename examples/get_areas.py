01c176331cd0ae8014fa1efb359fed92f9ad11d7
PhiBo <phibo@dinotools.org>
2016-11-22 22:05:19 +0100

    example - Get areas by name and show tag names and values

---
+++ examples/get_areas.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import overpy
+
+api = overpy.Overpass()
+
+# fetch all areas
+# More info on http://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_API_by_Example
+result = api.query("""
+area[name="Troisdorf"];
+out;
+""")
+
+for area in result.areas:
+    print(
+        "Name: %s (%i)" % (
+            area.tags.get("name", "n/a"),
+            area.id
+        )
+    )
+    for n, v in area.tags.items():
+        print("  Tag: %s = %s" % (n, v))
