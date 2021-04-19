628af20c53aeca5e7bafdd560f461107b2d981d6
PhiBo <phibo@dinotools.org>
2021-04-19 23:08:46 +0200

    build - Move info to static config and add urls

--- setup.py
+++ setup.py
@@ -7,36 +7,17 @@ HERE = Path(__file__).resolve().parent
 about = {}
 exec((HERE / "overpy" / "__about__.py").read_text(), about)
 
-long_description = (HERE / "README.rst").read_text()
-
 setup(
     name=about["__title__"],
     version=about["__version__"],
 
     description=about["__summary__"],
-    long_description=long_description,
     license=about["__license__"],
     url=about["__uri__"],
-
-    zip_safe=False,
     author=about["__author__"],
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy"
-    ],
     keywords="OverPy Overpass OSM OpenStreetMap",
     install_requires=[],
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests"]),
-    include_package_data=True,
     package_data={
         # "": ["README"],
     },
