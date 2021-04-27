DinoTools/python-overpy

commit 1450a0ce88756a73dbaa5cb083a495ea0748065f

Fix import order

docs/source/conf.py
@@ -7,6 +7,8 @@
 import sys
 from pathlib import Path
 
+from overpy import __about__ as overpy_about
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -49,7 +51,6 @@ copyright = '2014, PhiBo'
 # built documents.
 #
 # The short X.Y version.
-from overpy import __about__ as overpy_about
 version = overpy_about.__version__
 # The full version, including alpha/beta/rc tags.
 release = overpy_about.__version__

overpy/__init__.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 from datetime import datetime
 from decimal import Decimal
+from urllib.request import urlopen
+from urllib.error import HTTPError
 from xml.sax import handler, make_parser
 import json
 import re
@@ -24,9 +26,6 @@ GLOBAL_ATTRIBUTE_MODIFIERS = {
     "version": int,
     "visible": lambda v: v.lower() == "true"
 }
-
-from urllib.request import urlopen
-from urllib.error import HTTPError
 
 
 def is_valid_type(element, cls):

commit b569c843485477d727b2455b6e3c2a2448a5b550

doc - Fix import error introduced by 1450a0c
* See: 1450a0ce88756a73dbaa5cb083a495ea0748065f

docs/source/conf.py
@@ -7,7 +7,6 @@
 import sys
 from pathlib import Path
 
-from overpy import __about__ as overpy_about
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -51,10 +50,15 @@ copyright = '2014, PhiBo'
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
-# The short X.Y version.
-version = overpy_about.__version__
-# The full version, including alpha/beta/rc tags.
-release = overpy_about.__version__
+try:
+    from overpy import __about__ as overpy_about
+    # The short X.Y version.
+    version = overpy_about.__version__
+    # The full version, including alpha/beta/rc tags.
+    release = overpy_about.__version__
+except ImportError:
+    version = "unknown"
+    release = version
 
 # The language for content autogenerated by Sphinx. Refer to documentation
 # for a list of supported languages.